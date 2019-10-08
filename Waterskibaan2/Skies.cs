1919127c73c78d04b187afdc23071e38844f0e01
Martin Steenbergen <martin.k.steenbergen@gmail.com>
2019-10-08 19:38:52 +0200

    classes zwemvest en skies public gemaakt

--- Waterskibaan2/Skies.cs
+++ Waterskibaan2/Skies.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Waterskibaan2
 {
-    class Skies
+    public class Skies
     {
     }
 }
--- Waterskibaan2/Zwemvest.cs
+++ Waterskibaan2/Zwemvest.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Waterskibaan2
 {
-    class Zwemvest
+    public class Zwemvest
     {
     }
 }
